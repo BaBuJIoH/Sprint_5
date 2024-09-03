56b5d60c583b607c930aa34dad292cc225daa6f4
Aleksandr Kurbakov <kursasha@yandex.ru>
2024-09-03 20:36:01 +0300

    Corrected and moved the conftest.py to the root

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from selenium import webdriver
-
-@pytest.fixture  # Фикстура инициализации браузера
-def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    return driver
